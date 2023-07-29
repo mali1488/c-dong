0e3abbe2b48e5d20bea2ca75021c26d52ab708f7
mali1488 <mattias.linder87@gmail.com>
2023-07-29 22:35:36 +0200

    Trying out

--- cdong.c
+++ cdong.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "deps/raylib.h"
 
+
+
 int main(void) {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(450, 800, "CDong");
+    SetTargetFPS(60);
+    
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        int width = GetRenderWidth();
+        int height = GetRenderHeight();
+        {
+            ClearBackground(BLACK);
+            const char* msg = "CDong";
+            int msgWidth = MeasureText(msg, 20);
+            DrawText(msg, width/2 - msgWidth/2, height/2, 20, LIGHTGRAY);
+        }
         EndDrawing();
     }
 
