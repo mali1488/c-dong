be3f38840c074be4809fd83e1b62f5ed34ef0914
mali1488 <mattias.linder87@gmail.com>
2023-07-29 22:20:32 +0200

    Added raylib.c

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-clang cdong.c -o build/cdong
+clang cdong.c deps/libraylib.a -o build/cdong  -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL 
--- cdong.c
+++ cdong.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include "deps/raylib.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    InitWindow(800, 450, "raylib [core] example - basic window");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
