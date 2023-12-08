00693a0d7bf27f35b10a7fb756368eb7ce500c8e
mali1488 <mattias.linder87@gmail.com>
2023-12-08 23:10:42 +0100

    render paddles

--- cdong.c
+++ cdong.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "deps/raylib.h"
+
+#define PADDLE_WIDTH 125
+#define PADDLE_HEIGHT 15
+#define COLOR_BACKGROUND BLACK
+#define COLOR_FOREGROUND RAYWHITE
+
+typedef struct {
+    int x;
+} Paddle;
+
+typedef struct {
+    Paddle player_one_paddle;
+    Paddle player_two_paddle;
+} Game;
+
+static bool gameStart = false;
 
 void render_start_button() {
     int width = GetRenderWidth();
@@ -18,9 +35,9 @@ void render_start_button() {
     Vector2 mousePos = GetMousePosition();
     if (CheckCollisionPointRec(mousePos, button)) {
         if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-            printf("Start game!\n");
+            gameStart = true;
         }
-        DrawRectangleRec(button, RAYWHITE);
+        DrawRectangleRec(button, COLOR_FOREGROUND);
     } else {
         DrawRectangleRec(button, GRAY);
     }
@@ -38,22 +55,71 @@ void render_game_title() {
     render_label("C-Dong", width/2, height/2 - 40, 40, LIGHTGRAY);
 }
 
+void render_menu() {
+    BeginDrawing();
+    {
+        ClearBackground(BLACK);
+        render_game_title();
+        render_start_button();
+    }
+    EndDrawing();
+}
+
+void render_game(Game game) {
+    int width = GetRenderWidth();
+    int height = GetRenderHeight();
+    const int paddle_margin = 10;
+
+    BeginDrawing();
+    {
+        ClearBackground(COLOR_BACKGROUND);
+        Rectangle player_one = {
+            .x = game.player_one_paddle.x,
+            .y = 0 + paddle_margin,
+            .width = PADDLE_WIDTH,
+            .height = PADDLE_HEIGHT
+        };
+        DrawRectangleRec(player_one, COLOR_FOREGROUND);
+        Rectangle player_two = {
+            .x = game.player_two_paddle.x, 
+            .y = height - PADDLE_HEIGHT - paddle_margin, 
+            .width = PADDLE_WIDTH, 
+            .height = PADDLE_HEIGHT
+        };
+        DrawRectangleRec(player_two, COLOR_FOREGROUND);
+    }
+    EndDrawing();
+}
+
+void update_game_state(Game *game) {
+    const int vel = 2;
+    if (IsKeyDown(KEY_LEFT)) {
+        game->player_one_paddle.x -= vel;
+    }
+    if (IsKeyDown(KEY_RIGHT)) {
+        game->player_one_paddle.x += vel;
+    }
+    if (IsKeyDown(KEY_A)) {
+        game->player_two_paddle.x -= vel;
+    }
+    if (IsKeyDown(KEY_D)) {
+        game->player_two_paddle.x += vel;
+    }
+}
+
 int main(void) {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(450, 800, "CDong");
     SetTargetFPS(60);
     
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        {
-            ClearBackground(BLACK);
-            render_game_title();
-            render_start_button();
-        }
-        EndDrawing();
+    while (!WindowShouldClose() && !gameStart) {
+        render_menu();
     }
-
+    Game game = {0};
+    while (!WindowShouldClose()) {
+        update_game_state(&game);
+        render_game(game);
+    }
     CloseWindow();
     return 0;
 }
