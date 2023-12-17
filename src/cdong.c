7fc3e5dce21e988688e234353f1ea2572785003b
mali1488 <mattias.linder87@gmail.com>
2023-12-17 10:40:30 +0100

    reset ball

--- src/cdong.c
+++ src/cdong.c
@@ -15,16 +15,61 @@
 #define GAME_PADDLE_VELOCITY 2
 
 typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+} Ball;
+
+typedef struct {
     Rectangle player_one_paddle;
     Rectangle player_two_paddle;
-    Vector2 ball_position;
-    Vector2 ball_velocity;
+    Ball ball;
 } Game;
 
 static bool gameStart = false;
 
 void game_start() {
     gameStart = true;
+}
+
+Ball game_init_ball(bool up) {
+    int width = GetRenderWidth();
+    int height = GetRenderHeight();
+    Ball ball = {
+        .velocity = {
+            .x = 100,
+            .y = up ? -GAME_BALL_VELOCITY : GAME_BALL_VELOCITY
+        },
+        .position = {
+            .x = width / 2 - GAME_BALL_RADIUS / 2,
+            .y = height / 2 - GAME_BALL_RADIUS / 2
+        }
+    };
+    return ball;
+}
+
+Game game_init() {
+    Game game = {0};
+    int width = GetRenderWidth();
+    int height = GetRenderHeight();
+    game.ball = game_init_ball(false);
+
+    const float player_x_pos = width / 2 - GAME_PADDLE_WIDTH / 2;
+    const int paddle_margin = 10;
+    Rectangle player_one_paddle = {
+        .x = player_x_pos,
+        .y = paddle_margin,
+        .width = GAME_PADDLE_WIDTH,
+        .height = GAME_PADDLE_HEIGHT
+    };
+    game.player_one_paddle = player_one_paddle;
+     Rectangle player_two_paddle = {
+        .x = player_x_pos,
+        .y = height - paddle_margin - GAME_PADDLE_HEIGHT,
+        .width = GAME_PADDLE_WIDTH,
+        .height = GAME_PADDLE_HEIGHT
+    };
+    game.player_two_paddle = player_two_paddle;
+    return game;
 }
 
 void game_render_paddle(Rectangle paddle) {
@@ -41,7 +86,7 @@ void game_render(Game game) {
         ClearBackground(COLOR_BACKGROUND);
         game_render_paddle(game.player_one_paddle);
         game_render_paddle(game.player_two_paddle);
-        game_render_ball(game.ball_position);
+        game_render_ball(game.ball.position);
     }
     EndDrawing();
 }
@@ -74,47 +119,21 @@ void game_update_state(Game *game) {
         game_move_paddle_right(&game->player_two_paddle);
     }
     if (
-        CheckCollisionCircleRec(game->ball_position, GAME_BALL_RADIUS, game->player_one_paddle) ||
-        CheckCollisionCircleRec(game->ball_position, GAME_BALL_RADIUS, game->player_two_paddle)
+        CheckCollisionCircleRec(game->ball.position, GAME_BALL_RADIUS, game->player_one_paddle) ||
+        CheckCollisionCircleRec(game->ball.position, GAME_BALL_RADIUS, game->player_two_paddle)
     ) {
-        game->ball_velocity.y *= -1 * 1.25;
+        game->ball.velocity.y *= -1 * 1.25;
     }
-    if (game->ball_position.x - GAME_BALL_RADIUS < 0 || game->ball_position.x + GAME_BALL_RADIUS > width) {
-        game->ball_velocity.x *= -1;
+    if (game->ball.position.x - GAME_BALL_RADIUS < 0 || game->ball.position.x + GAME_BALL_RADIUS > width) {
+        game->ball.velocity.x *= -1;
     }
-    if (game->ball_position.y - GAME_BALL_RADIUS < 0 || game->ball_position.y + GAME_BALL_RADIUS > height) {
-        game->ball_velocity.y *= -1;
+    if (game->ball.position.y - GAME_BALL_RADIUS < 0) {
+        game->ball = game_init_ball(false);
+    } else if (game->ball.position.y + GAME_BALL_RADIUS > height) {
+        game->ball = game_init_ball(true);
     }
-    game->ball_position.x += game->ball_velocity.x * dt;
-    game->ball_position.y += game->ball_velocity.y * dt;
-}
-
-Game game_init() {
-    Game game = {0};
-    int width = GetRenderWidth();
-    int height = GetRenderHeight();
-    game.ball_velocity.x = 100;
-    game.ball_velocity.y = -GAME_BALL_VELOCITY;
-    game.ball_position.x = width / 2 - GAME_BALL_RADIUS / 2;
-    game.ball_position.y = height / 2 - GAME_BALL_RADIUS / 2;
-
-    const float player_x_pos = width / 2 - GAME_PADDLE_WIDTH / 2;
-    const int paddle_margin = 10;
-    Rectangle player_one_paddle = {
-        .x = player_x_pos,
-        .y = paddle_margin,
-        .width = GAME_PADDLE_WIDTH,
-        .height = GAME_PADDLE_HEIGHT
-    };
-    game.player_one_paddle = player_one_paddle;
-     Rectangle player_two_paddle = {
-        .x = player_x_pos,
-        .y = height - paddle_margin - GAME_PADDLE_HEIGHT,
-        .width = GAME_PADDLE_WIDTH,
-        .height = GAME_PADDLE_HEIGHT
-    };
-    game.player_two_paddle = player_two_paddle;
-    return game;
+    game->ball.position.x += game->ball.velocity.x * dt;
+    game->ball.position.y += game->ball.velocity.y * dt;
 }
 
 int main(void) {
