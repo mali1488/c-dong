a2c7a676eb7f134fa7e68047a579cb9744125b4a
mali1488 <mattias.linder87@gmail.com>
2023-12-28 22:14:31 +0100

    make it size independent

--- src/cdong_ui.h
+++ src/cdong_ui.h
@@ -13,24 +13,28 @@ int _render_label(const char* text, int x, int y, int font_size, Color c) {
     return width;
 }
 
-bool _render_start_button() {
+bool render_start_button() {
     const int width = GetScreenWidth();
     const int height = GetScreenHeight();
     
     const int margin = 5;
-    const int padding = 10;
     const char* msg = "Start";
-    const int msgWidth = MeasureText(msg, 20);
+    const float font_size = width * 0.05;
+    const int msg_width = MeasureText(msg, font_size);
+    const float rect_pos_y = height / 2 + margin;
+    const float rect_pos_x = (width - msg_width) / 2;
+    const padding = msg_width * 0.1;
+    
     Rectangle button = {
-        .x = (width - msgWidth - padding) / 2,
-        .y = height / 2 + margin,
-        .width = msgWidth + padding,
-        .height = 20 + padding
+        .x = rect_pos_x,
+        .y = rect_pos_y,
+        .width = msg_width + padding,
+        .height = font_size + padding
     };
     Vector2 mousePos = GetMousePosition();
     bool mousePressed = false;
-    Color background_color = COLOR_FOREGROUND;
-    Color text_color = BLACK;
+    Color background_color = BLUE;
+    Color text_color = WHITE;
     if (CheckCollisionPointRec(mousePos, button)) {
         if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
             mousePressed = true;
@@ -39,14 +43,14 @@ bool _render_start_button() {
         text_color = COLOR_FOREGROUND;
     }
     DrawRectangleRec(button, background_color);
-    DrawText(msg, (width - msgWidth) / 2, (height + padding) / 2 + margin, 20, text_color);
+    DrawText(msg, rect_pos_x + padding / 2, rect_pos_y + padding / 2, font_size, text_color);
     return mousePressed;
 }
 
-void _render_game_title() {
+void render_game_title() {
     const int width = GetScreenWidth();
     const int height = GetScreenHeight();
-    const int font_size = 40;
+    const int font_size = width * 0.1;
     const int w = MeasureText(GAME_TITLE, font_size);
     DrawText(GAME_TITLE, width / 2 - w / 2, height / 2 - font_size, font_size, COLOR_FOREGROUND);
 }
@@ -56,8 +60,8 @@ bool ui_start_menu() {
     BeginDrawing();
     {
         ClearBackground(BLACK);
-        _render_game_title();
-        if (_render_start_button()) {
+        render_game_title();
+        if (render_start_button()) {
             game_start = true;
         }
     }
